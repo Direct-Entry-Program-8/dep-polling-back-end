bf03f23aa7c063cf4e4f99f65ddeb70a1f3a5d72
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-10 11:31:36 +0530

    Finalize entities

--- src/main/java/lk/ijse/dep8/polling/entity/Poll.java
+++ src/main/java/lk/ijse/dep8/polling/entity/Poll.java
@@ -1,4 +1,25 @@
 package lk.ijse.dep8.polling.entity;
 
-public class Poll {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Poll implements SuperEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String title;
+    @Column(name = "up_votes", nullable = false)
+    private int upVotes;
+    @Column(name = "down_votes", nullable = false)
+    private int downVotes;
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
 }
