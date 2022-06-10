31ebb09f0e43ee4ce10ab3d1a94b8b3aa9d71261
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-10 14:09:06 +0530

    Implement PollServlet - 1

---
+++ src/main/java/lk/ijse/dep8/polling/listener/AppListener.java
@@ -0,0 +1,14 @@
+package lk.ijse.dep8.polling.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class AppListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.setProperty("app.profiles.active", "dev");
+    }
+}
