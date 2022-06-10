f0040228a72a33fd06b06c51b54f4804f35c7012
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-10 16:52:43 +0530

    Add PollServiceImpl

---
+++ src/main/java/lk/ijse/dep8/polling/service/custom/impl/PollServiceImpl.java
@@ -0,0 +1,34 @@
+package lk.ijse.dep8.polling.service.custom.impl;
+
+import lk.ijse.dep8.polling.dto.PollDTO;
+import lk.ijse.dep8.polling.service.custom.PollService;
+
+import java.util.List;
+
+public class PollServiceImpl implements PollService {
+
+    @Override
+    public List<PollDTO> listAllPolls() {
+        return null;
+    }
+
+    @Override
+    public PollDTO getPoll(int id) {
+        return null;
+    }
+
+    @Override
+    public PollDTO savePoll(PollDTO dto) {
+        return null;
+    }
+
+    @Override
+    public void updatePoll(PollDTO dto) {
+
+    }
+
+    @Override
+    public void deletePoll(int id) {
+
+    }
+}
