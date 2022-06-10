c816d64ab80fa15c1337853155df04042cdcf739
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-10 17:09:54 +0530

    Test EntityDTOTransformer

---
+++ src/main/java/lk/ijse/dep8/polling/service/util/EntityDTOTransformer.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep8.polling.service.util;
+
+import lk.ijse.dep8.polling.dto.PollDTO;
+import lk.ijse.dep8.polling.entity.Poll;
+import org.modelmapper.ModelMapper;
+
+public class EntityDTOTransformer {
+
+    public static PollDTO getPollDTO(Poll pollEntity){
+        ModelMapper mapper = new ModelMapper();
+        return mapper.map(pollEntity, PollDTO.class);
+    }
+
+    public static Poll getPoll(PollDTO pollDTO){
+        ModelMapper mapper = new ModelMapper();
+        return mapper.map(pollDTO, Poll.class);
+    }
+
+}
